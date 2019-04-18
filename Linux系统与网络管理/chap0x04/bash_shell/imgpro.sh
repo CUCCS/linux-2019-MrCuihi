3205fbc6646458323f8358729f115cf77e3cd886
MrCuihi <649249762@qq.cm>
2019-04-18 11:51:42 +0800

    chap0x04 Shell脚本编程

--- Linux系统与网络管理/chap0x04/bash_shell/imgpro.sh
+++ Linux系统与网络管理/chap0x04/bash_shell/imgpro.sh
@@ -4,45 +4,43 @@
 
 function usage()
 {
-echo " IMproved 1.0 (2019 April 2)\n"
-
-echo " usage: bash imgpro [Arguments][][][]\n"
+echo " IMproved 1.0 (2019 April 2)"
+echo " usage: bash imgpro [Arguments][][][]"
 echo " Arguments: "
-echo " -j  [source(.png)] [destination(.jpg)] : convert png/svg to jpg"
-echo " -n  [pattern] [replacement] : Rename files based on input batch"
-echo " -q  [quality] [source.jpg] [destination.jpg] : Image quality compression for jpeg format images"
-echo " -r  [%|(size)x(size)] [source.jpg|png] [destination.jpg|png] :Compress images while maintaining the same height and width (please add/use %)"
-echo " -w  [filename.jpg] [watermark] : Embed a custom watermark (please add/use *.jpg batch)"
+echo " -q  [quality] [source.jpeg] [destination.jpeg] : Image quality compression for jpeg format images"
+echo " -r  [%|(size)x(size)] [source.jpg|png] [destination.jpeg|png] :Compress images while maintaining the same height and width (please add/use %)"
+echo " -w  [filename.jpeg] [watermark] : Embed a custom watermark (please add/use *.jpeg batch)"
+echo " -m  [pattern] [replacement] : Rename files based on input batch"
+echo " -c  [source(.png)] [destination(.jpeg)] : convert png/svg to jpeg"
 echo " -h  Output help information"
 
 }
 
 function Process()
 { 
- while [ "$1" != "" ];do
+ if [ "$1" != "" ];then
   #convert png/svg to jpg
-  if [ "$1" == "-j" ];then
+  if [ "$1" == "-c" ];then
      if [ $# == 3 ];then
-        if [ ! -f $2.png ];then
+        if [ ! -f "$2" ];then
            echo "No such file"
            exit 1
         else
-           if [ `file --mime-type -b $2.png` == "image/png"  ];then
-              $(convert $2.png $3.jpg)
+	   if [ `file --mime-type -b $2` == "image/png" ];then
               #convert successfully
-              if [ $?==1 ];then
+	      $(convert $2 $3)
+              if [ $? == 1 ];then
                 echo "Conversion failed."
                 exit 1
               else
                 echo "Successfully converted png/svg to jpg."
-                echo "Source: $(file $2.png)"
-                echo "Destination: $(file $3.jpg)" 
+                echo "Source: $(file $2)"
+                echo "Destination: $(file $3)" 
                 exit 0
               fi
-            else
-               echo "Please provide png/svg pictures."
-               exit 1
-            fi
+	     else
+                echo "Please provide png/svg pictures."
+	     fi
          fi
       else
          #Eorror , output usage information
@@ -51,16 +49,16 @@ function Process()
       fi
       
   # Rename files based on input batch
-  elif [ "$1" == "-n" ];then
-      if [ $# == 3];then
+  elif [ "$1" == "-m" ];then
+      if [ $# == 3 ];then
         $(rename s'/'$2'/'$3'/' *)
         if [ $? == 1 ];then
            echo "Batch rename file failed."
            exit 1
         else
            echo "Batch renamed file successfully."
-           echo "Source: $2"
-           echo "Destination: $3"
+	   echo "Source: $(file $2)"
+	   echo "Destination: $(file $3)"
            exit 0
         fi
       #Eorror , output usage information
@@ -76,7 +74,7 @@ function Process()
               echo "No such file"
               exit 1
            else
-              #only jpeg 
+              #only jpeg
               if [ `file --mime-type -b $3` == "image/jpeg" ];then
                  $(convert -quality $2 $3 $4)
                  if [ $? == 1 ];then
@@ -84,8 +82,8 @@ function Process()
                    exit 1
                  else
                    echo "Jpeg image quality compression succeeded"
-                   echo "Source: $(file $3.jpeg)"
-                   echo "Destination: $(file $4.jpg)"
+                   echo "Source: $(file $3)"
+                   echo "Destination: $(file $4)"
                    exit 0
                  fi
               else
@@ -113,8 +111,8 @@ function Process()
                 exit 1
               else
                 echo "Compress images while maintaining the same height and width succeeded."
-                echo "Source: $(file $3.jpeg)"
-                echo "Destination: $(file $4.jpg)"
+                echo "Source: $(file $3)"
+                echo "Destination: $(file $4)"
                 exit 0
               fi
             else
@@ -137,8 +135,8 @@ function Process()
               exit 1
             else
               echo "Embedding a custom watermark successfully."
-              echo "Source: $2"
-              echo "Destination: $3"
+	      echo "Source: (file $2)"
+	      echo "Destination:(file $3)"
               exit 0
             fi
         else
@@ -152,9 +150,9 @@ function Process()
   else
      usage
 
-  if
-  
-done 
+  fi
+ 
+ fi
              
 }
 
